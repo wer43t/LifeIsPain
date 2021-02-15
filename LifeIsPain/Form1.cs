bdd8f56c9f926388da0fe6e0785c2d7189607483
Злой Чай, который Салават <abrarov03@gmail.com>
2021-02-16 01:31:46 +0300

    Добавьте файлы проекта.

---
+++ LifeIsPain/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifeIsPain
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int resolution;
+        GameEngine gameEngine;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DrawNextGeneration();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                return;
+
+
+            nudResolution.Enabled = false;
+            nudDensity.Enabled = false;
+            resolution = Convert.ToInt32(nudResolution.Value);
+
+            gameEngine = new GameEngine
+                (
+                rows: pictureBox1.Height / resolution,
+                cols: pictureBox1.Width / resolution,
+                density: (int)nudDensity.Minimum + (int)nudDensity.Maximum - (int)nudDensity.Value
+                );
+
+            Text = $"Generation {gameEngine.CurrentGeneration}";
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+
+            timer1.Start();
+        }
+
+        private void DrawNextGeneration()
+        {
+            graphics.Clear(Color.Black);
+
+            var field = gameEngine.GetCurrentGeneration();
+
+            for (int x = 0; x < field.GetLength(0); x++)
+            {
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    if (field[x,y])
+                    graphics.FillRectangle(Brushes.White,
+                                               x * resolution,
+                                               y * resolution,
+                                               resolution-1, resolution-1);
+                }
+            }
+
+            pictureBox1.Refresh();
+
+            Text = $"Generation {gameEngine.CurrentGeneration}";
+
+            gameEngine.NextGeneration(); // миша всё х-ня давай по новой
+        }
+
+        private void StopGame()
+        {
+            if (!timer1.Enabled)
+                return;
+            timer1.Stop();
+            nudResolution.Enabled = true;
+            nudDensity.Enabled = true;
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            StopGame();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                gameEngine.AddCell(x, y);
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolution;
+                var y = e.Location.Y / resolution;
+                gameEngine.RemoveCell(x, y);
+            }
+        }
+    }
+}
---
+++ LifeIsPain/GameEngine.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeIsPain
+{
+    public class GameEngine
+    {
+        public uint CurrentGeneration { get; private set; }
+        private bool[,] field;
+        private readonly int cols;
+        private readonly int rows;
+        private Random rnd = new Random();
+
+        public GameEngine(int rows, int cols, int density)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            field = new bool[cols, rows];
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = rnd.Next(density) == 0;
+                }
+            }
+        }
+
+        private int SumOfNeighbors(int x, int y)
+        {
+            int cnt = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    int col = (x + i + cols) % cols;
+                    int row = (y + j + rows) % rows;
+
+                    var isSelfChecking = col == x && row == y; // скипаем клетку саму себя
+                    var hasLife = field[col, row];
+
+                    if (hasLife && !isSelfChecking)
+                        cnt++;
+                }
+            }
+
+            return cnt;
+        }
+
+        public void NextGeneration()
+        {
+            var newField = new bool[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    int neighboursSum = SumOfNeighbors(x, y);
+                    bool hasLife = field[x, y]; // Есть живая клетка чи нет
+
+                    if (!hasLife && neighboursSum == 3)
+                        newField[x, y] = true;
+                    else if (hasLife && (neighboursSum < 2 || neighboursSum > 3))
+                        newField[x, y] = false;
+                    else
+                        newField[x, y] = field[x, y];
+                }
+            }
+
+            field = newField;
+            CurrentGeneration++;
+        }
+
+        public bool[,] GetCurrentGeneration() // создаю кароче копию своего массива,
+            //чтобы не нарушить инкапсуляцию и логику игры
+        {
+            var result = new bool[cols, rows];
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    result[x, y] = field[x, y];
+                }
+            }
+            return result;
+        }
+
+        private bool ValidateCellPosition(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < cols && y < rows;
+        }
+
+        private void UpdateCell(int x, int y, bool state)
+        {
+            if (ValidateCellPosition(x, y))
+                field[x, y] = state;
+        }
+        
+        public void AddCell(int x, int y)
+        {
+            UpdateCell(x, y, state: true);
+        }
+
+        public void RemoveCell(int x, int y)
+        {
+            UpdateCell(x, y, state : false);
+        }
+    }
+}
+ 
